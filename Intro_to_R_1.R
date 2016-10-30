8cb5504d5f6cee7590f3cd82763edced9dcf8a21
eecampbell <Eleanor.Campbell@unh.edu>
2016-10-30 08:18:07 -0400

    Edits to Session 1 script

--- Intro_to_R_1.R
+++ Intro_to_R_1.R
@@ -1,6 +1,6 @@
 #R Script paired with Introduction to R
 #series for the Trace Gas Biogeochem Lab
-#Last updated 10/28/2016
+#Last updated 10/30/2016
 #created by Nell Campbell
 
 #clear workspace
@@ -19,7 +19,7 @@ setwd("C:/Users/Tom/Documents/GitHub/RScriptsLab")
 ex_num_scalar<- 3
 ex_char_scalar<- "hello"
 
-#check that scalars objects contain the information you expect
+#check scalar objects
 ex_num_scalar
 ex_char_scalar
 
@@ -30,56 +30,54 @@ ex_char_scalar
 ex_num_vector<- c(3,5,7,19,22)
 ex_char_vector<- c("hello","there","world")
 
-#check that vector objects contain the information you expect
+#check vector objects
 ex_num_vector
 ex_char_vector
 
 #information in vector can be referenced by location in list
-ex_num_vector[3] #should print value 7
+ex_num_vector[3] #prints value 7
 ex_char_vector[c(1,3)] #calls 1st and 3rd place in vector, printing "hello" "world"
 
 #####MATRIX
-#references to matrix cells is always ordered: rows, columns
+#references to matrix cells is ordered: rows, columns
 #matrices must have all of the same type of data, e.g. 'numeric', or 'character'
 
 #step one, create a matrix of 2 rows and 12 columns, filled with the value '0'
 ex_matrix<- matrix(0, nrow=2, ncol=12)
 
-#check that matrix was created properly
+#check matrix object
 ex_matrix
 
 #step two, fill the first row with a sequence of values from 1 to the value of the
 #total number of columns (in this case, ncol(ex_matrix)=12)
 
-#let's check that we know how to make a sequence like this using the 'seq' function:
+#check sequence function is set up correctly
 #seq(from=start of sequence, to=end of sequence, by=units of sequence) 
 seq(from=1,to=ncol(ex_matrix), by=1)
 
-#now, fill in the first row of the matrix with these values
+#fill in the first row of the matrix with these values
 ex_matrix[1,]<- seq(from=1,to=ncol(ex_matrix), by=1)
 
-#what does the matrix look like now?
+#check matrix object
 ex_matrix
 
-#now let's fill the second row with randomly selected values
+#let's fill the second row with randomly selected values
 #from a normal distribution, one for each column
 
-#we'll use the rnorm function to randomly select numbers, which looks like:
+#we'll use the rnorm function, which looks like:
 #rnorm(x=number of points, mean=mean of points, sd=standard deviation of points) 
 #first, check that the function is set up properly
-#remember, ncol(ex_matrix)<-12 in this example
 rnorm(n=ncol(ex_matrix), mean=3, sd=.5)
 
-#now, run that same code to fill in second row of matrix
+#now, fill in second row of matrix
 ex_matrix[2,]<-rnorm(n=ncol(ex_matrix), mean=3, sd=.5)
 
-#what does the matrix look like now?
+#check matrix object
 ex_matrix
 
-#let's plot the results
-#at its most basic, the plot function needs: plot(x=x values, y=y values)
-#plot function is very flexible. Here I want the type to be line, and the 
-#line to be red
+#plot results
+#the plot function works with the minimum of x and y values: plot(x=x values, y=y values)
+#but plotting is flexible. Here I want the type to be line, and the line to be red
 plot(x=ex_matrix[1,], y=ex_matrix[2,], typ="l", col="red")
 
 
